231f1799036a7044a334e030c0e2bab18ab48cc0
Ronnie <rix17172@gmail.com>
2016-04-18 11:01:27 +0200

    Update messageparser.php

--- include/messageparser.php
+++ include/messageparser.php
@@ -6,5 +6,23 @@ class MessageParser{
      $first = explode(" ", substr($message, 0, strpos($message, ": ")));
      $this->data["command"] = $first[0];
      $this->data["isCommand"] = count($first) == 0;
+     $this->data["channel"] = $this->isCommand() ? get_channel($first[1]) : null;
+     $this->data["message"] = substr($message, strpos($message, ": ")+2);
+  }
+
+  function isCommand(){
+     return $this->data["isCommand"];
+  }
+
+  function command(){
+      return $this->data["command];
+  }
+
+  function channel(){
+     return $this->data["channel"];
+  }
+
+  function message(){
+     return $this->data["message"];
   }
 }
