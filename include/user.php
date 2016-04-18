8ed422c5f7c7e5af4da766078ffb65b6fa9d7465
Ronnie <rix17172@gmail.com>
2016-04-18 11:50:08 +0200

    Create user.php

---
+++ include/user.php
@@ -0,0 +1,20 @@
+class User{
+  private static $users = [];
+  private static $current = null;
+
+  public static function push($data, $setCurrent = false){
+     //wee control that the user is not set in our cache system
+     if(empty(self::$users[$data["id"]])){
+       //push the user to the cahce system 
+       self::$users[$data["id"]] = new UserData($data);
+     }
+
+     if($setCurrent)
+       self::$current = self::$users[$data["id"]];
+  }
+
+}
+
+class UserData{
+
+}
