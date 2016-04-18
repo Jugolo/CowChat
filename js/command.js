c4a8bf0477996f3e9bdbf81c007eb4b63baa4133
Ronnie <rix17172@gmail.com>
2016-04-19 01:06:12 +0200

    Update command.js

--- js/command.js
+++ js/command.js
@@ -1,3 +1,5 @@
+var prefixCache = [];
+
 function rand(min, max){
     Math.floor((Math.random() * max) + min);
 }
@@ -9,6 +11,37 @@ var number = 0;
 function createPrefix(){
   if(letterNumber >= letter.length){
     letterNumber=0;
-    return createPrefix;
+    return createPrefix();
   }
+
+  var l = letter.charAt(letterNumber);
+
+  var n = number.toString();
+  number++;
+  if(n.length == 1){
+    return l+"0000"+n;
+  }else if(n.length == 2){
+    return l+"000"+n;
+  }else if(n.length == 3){
+    return l+"00"+n;
+  }else if(n.length == 4){
+    return l+"0"+n;
+  }else if(n.length == 5){
+    return l+n;
+  }
+
+  number = 1;
+  letterNumber++;
+  return l+"0001";
+}
+
+
+function join(channel, callback){
+  if(typeof channel === "array"){
+    channel = channel.join(",");
+  }
+
+  var prefix = createPrefix();
+  prefixCache[prefix] = callback;
+  send(prefix+"JOIN: "+channel;
 }
