aee423286e9a28b0b911769d5892db905ee038c4
Ronnie <rix17172@gmail.com>
2016-04-19 21:41:30 +0200

    Update message.php

--- include/message.php
+++ include/message.php
@@ -6,3 +6,23 @@ function send(MessageParser $parser, $message){
 
    echo $message."\r\n";
 }
+
+function send_channel(ChannelData $channel, UserData $user, $message){
+    if(!$user->isMember($channel)){
+       if($user == User::current()){
+         send(new MessageParser(), "ERROR: notMember");
+       }
+       return false;
+    }
+    Database::insert("message",[
+        'uid'  => $user->id(),
+        'cid'  => $channel->id(),
+        'msg'  => $message,
+        'nick' => $user->nick()//if this is a geaust the user will be deletede so there for wee use this as a backup
+    ]);
+
+    if(Server::is_cli()){
+      WebSocket::send($channel, $message);
+    }
+    return true;
+}
