5259fcf3e312f28bf3e74a3a49335bb275dcb501
Ronnie <rix17172@gmail.com>
2016-04-26 10:00:37 +0200

    Create defender.php

---
+++ include/defender.php
@@ -0,0 +1,27 @@
+<?php
+
+class Defender{
+   private $data = [];
+
+   public function updateCount($data){
+      if($data > 0){
+         if(User::current()->defenderCount() >= 1){
+           return;
+         }
+         //wee plus it to the count and controle if it is over 1
+         $n = User::current()->defenderCount()+$data;
+         if($n > 1){
+            $n = 1;
+         }
+         User::current()->defenderCount($n);
+         return;
+      }
+
+      //wee controle if the new count is less end 0
+      $n = User::defenderCount()+$data;
+      if($n < 0){
+         FireWall::ban(time()*(60+(($n + 15) * 15)));
+      }
+      User::defenderCount($n);
+   }
+}
