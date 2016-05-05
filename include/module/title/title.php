821191f548dad8aa24eff560da9c5052ece3c401
Ronnie <rix17172@gmail.com>
2016-05-06 00:59:58 +0200

    Create title.php

---
+++ include/module/title/title.php
@@ -0,0 +1,13 @@
+<?php
+function title_command(MessagePaser $msg){
+   if(User::current()->isMember($msg->channel())){
+      if($msg->message() == ""){
+         title($msg, $msg->channel()->title(), false);
+      }else{
+         $msg->channel()->title($msg->message());
+         title($msg, $msg->channel()->title(), true);
+      }
+   }else{
+      error($msg, "You are not member of the channel");
+   }
+}
