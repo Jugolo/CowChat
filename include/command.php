1ddb7a632c8137976124c6942dcd637654c0ca81
Ronnie <rix17172@gmail.com>
2016-05-06 07:33:01 +0200

    Update command.php

--- include/command.php
+++ include/command.php
@@ -3,6 +3,9 @@ function error(MessageParser $parser, $message){
 	send($parser, "ERROR: ".$message);
 }
 
-function title(MessageParser $parser, $channel, $title){
-	send($parser, "TITLE ".$channel.": ".$title);
+function title(MessageParser $parser, $channel, $title, $globel){
+        if($globel)
+           Channel::get($channel)->send("TITLE ".$channel.": ".$title);
+        else
+	   send($parser, "TITLE ".$channel.": ".$title);
 }
