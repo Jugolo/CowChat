0fe46bf3ea0116ac1e556bf9d1e0e8b02cd5dfdd
Ronnie <rix17172@gmail.com>
2016-05-07 09:14:00 +0200

    Update info.php

--- include/module/info/info.php
+++ include/module/info/info.php
@@ -3,9 +3,15 @@ function info_command(MessageParser $parser){
     if(User::current()->isMember($parser->channel())){
        //wee controle if the user is member of the channel
        if(($user = User::get($parser->message())) != null){
-
+           //wee controle the user is member of the channel
+           if($user->isMember($parser->channel())){
+              //okay wee send the user data to the user
+              send($parser, $parser->channel()->getMember($user)->group());
+           }else{
+              error($parser, "The user is not member of the channel");
+           }
        }else{
-          send($parser, "Unknown user");
+          error($parser, "Unknown user");
        }
     }else{
        error($parser, "You are not member of the channel);
