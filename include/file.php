c8f8071942d2f930466bda5eebe0b4e0e8cf7665
Ronnie <rix17172@gmail.com>
2016-05-09 10:00:19 +0200

    Create file.php

---
+++ include/file.php
@@ -0,0 +1,17 @@
+<?php
+
+class Files{
+  public static function changeDir($dir){
+     set_include_path($dir);
+     //to get file_exists to work and in websocket server wee need to change dir
+     chdir($dir);
+  }
+
+  public static function exists($name){
+     return file_exists($name);
+  }
+
+  public static function context($name){
+    return file_get_contents($name);
+  }
+}
