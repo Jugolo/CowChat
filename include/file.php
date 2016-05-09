8704bd28ca60e6f135e655fbf015fdc8d00f85df
Ronnie <rix17172@gmail.com>
2016-05-09 22:01:47 +0200

    Update file.php

--- include/file.php
+++ include/file.php
@@ -2,7 +2,7 @@
 
 class Files{
   public static function exists($name){
-     return @fopen($name, "r", true) == true;
+     return file_exists(getDir().$name);
   }
 
   public static function context($name){
@@ -10,7 +10,7 @@ class Files{
   }
 
   public static function isFile($name){
-     return @file($name, FILE_USE_INCLUDE_PATH) != false;
+     return is_file(getDir().$name);
   }
 }
 
@@ -26,18 +26,19 @@ function getDir(){
 	if($dir !== null){
 		return $dir;
 	}
-	
+	//exit(dirname($_SERVER["SCRIPT_FILENAME"])."|".CHAT_PATH);
 	//wee get the current dir and chat dir :)
 	$current_dir = str_replace("/", "\\", dirname($_SERVER["SCRIPT_FILENAME"])."\\");
-	if($current_dir == CHAT_PATH){
+        $chat_path   = str_replace("/", "\\", CHAT_PATH);
+	if($current_dir == $chat_path){
 		return $dir = "";
 	}
 	
-	if(($pos = strpos(CHAT_PATH, $current_dir)) !== false){
+	if(($pos = strpos($chat_path, $current_dir)) !== false){
 		return $dir = substr(CHAT_PATH, $pos);
 	}else{
 		//exit(CHAT_PATH."|".$current_dir);
-		$chat_path = explode("\\", CHAT_PATH);
+		$chat_path = explode("\\", $chat_path);
 		$current_dir = explode("\\", $current_dir);
 		if(count($current_dir) < count($chat_path)){
 			throw new Exception("A chat file must be in chat system: ".implode("\\", $current_dir));
@@ -53,6 +54,6 @@ function getDir(){
 			$path .= "../"; 
 		}
 		
-		return $path;
+		return $dir = $path;
 	}
 }
