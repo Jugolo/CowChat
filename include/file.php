e3f1be58019af2098f58e0b62a855df0e4f4c1c2
Ronnie <rix17172@gmail.com>
2016-05-09 11:23:04 +0200

    Update file.php

--- include/file.php
+++ include/file.php
@@ -8,6 +8,10 @@ class Files{
   public static function context($name){
     return file_get_contents($name, true);
   }
+
+  public static function isFile($name){
+     return is_file(get_include_path().$name);
+  }
 }
 
 class Dirs{
