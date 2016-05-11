7e72b1ce6c13603c2054cb972bd24f1d0c4c728e
Ronnie <rix17172@gmail.com>
2016-05-11 09:01:15 +0200

    Update nick.php

--- include/module/nick/nick.php
+++ include/module/nick/nick.php
@@ -1,5 +1,14 @@
 <?php
 
 function nick_command(MessageParser $msg){
+    if(controleNick($msg->message(), User::current()){
+       //okay the nick is free 
+       User::current()->renderChannels(function(ChannelData $data) use($msg){
+           $data->send("NICK ".$data->name().": ".$msg->message());
+       });
 
+       User::current()->nick($msg->message());
+    }else{
+       error($msg, "Nick is taken");
+    }
 }
