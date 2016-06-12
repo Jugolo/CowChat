d690c1a35e2b23bd975161ca40ad55a2cf2382db
Ronnie <rix17172@gmail.com>
2016-06-12 19:38:33 +0200

    Create setup.php

---
+++ include/setup.php
@@ -0,0 +1,21 @@
+<?php
+
+class Setup{
+  private $data;
+
+  public function __construct(){
+    if(Server::is_cli()){
+      exit("You can not start the chat server when the setup dir is online.\r\nPlease delte it or rename it (wee not recommend to rename)");
+    }
+
+    $this->data = json_decode(Files::context("setup/info.json"), true);
+
+    if(!$this->is_new()){
+      exit("You need to delete the setup dir to let the chat work");
+    }
+  }
+
+  private function is_new(){
+    return version_compare(CHAT_VERSION, $this->version(), ">");
+  }
+}
