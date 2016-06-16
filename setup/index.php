5db3e01f890651433a19a1c24310b22be58c6b93
Ronnie <rix17172@gmail.com>
2016-06-16 11:32:36 +0200

    Update index.php

--- setup/index.php
+++ setup/index.php
@@ -1,3 +1,10 @@
 <?php
-define("NO_CONTEXT", true);
-define("IN_SETUP", true);
+define("NO_CONTEXT", true);//wee do not wich to show context (will probably show login page)
+define("IN_SETUP", true);//tell the chat wee are in setup mode
+define("CHAT_PATH", "../".dirname(__FILE__) . '\\');//set the chat root dir so the system dont crach
+//controle if the config.json exists
+if(file_exists("../include/config.json")){
+   include "mode/finish.php";
+}else{
+   include "mode/begin.php";
+}
