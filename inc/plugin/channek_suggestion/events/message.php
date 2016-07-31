743ec743c88540f90dd23cb5a5caf40dfd808bd2
Ronnie <rix17172@gmail.com>
2016-07-31 22:25:48 +0200

    Update message.php

--- inc/plugin/channek_suggestion/events/message.php
+++ inc/plugin/channek_suggestion/events/message.php
@@ -1,5 +1,13 @@
 namespace inc\plugin\channel_suggestion\events\message;
 
 class MessageHandler implements Event{
+    public function getEvents() : array{
+        return [
+             "ajax.message.resive" => [$this, "messageGet"]
+        ];
+    }
 
+    public function messageGet(MessageParser $message){
+
+    }
 }
