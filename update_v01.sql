728138e142d5990d22d1930207a2873cc3319cf6
Ronnie <rix17172@gmail.com>
2017-02-12 00:04:14 +0100

    Create update_v01.sql

---
+++ update_v01.sql
@@ -0,0 +1,10 @@
+ALTER TABLE `%prefix%chat_user ADD ip VARCHAR(255) AFTER avatar;
+
+CREATE TABLE IF NOT EXISTS `%prefix%chat_plugin` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `type` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `dir` varchar(255) NOT NULL,
+  `method` varchar(255) NOT NULL,
+  PRIMARY KEY(`id`)
+) ENGINE=MyISAM;
