07647c1916c0976c3e921b2a698649ce337d12fb
Ronnie <rix17172@gmail.com>
2017-02-17 19:23:36 +0100

    Create plugin.php

---
+++ lib/plugin/Friends/plugin.php
@@ -0,0 +1,20 @@
+<?php
+
+class Friends_Plugin{
+   private $db;
+   
+   public function __construct(DatabaseHandler $db){
+      $this->db = $db;
+   }
+   
+   public function doInstall(){//trigged when the install is installed
+     $this->db->query("CREATE TABLE IF NOT EXISTS `".DB_PREFIX."chat_friends` (
+       `one` int(11) NOT NULL,
+       `two` int(11) NOT NULL
+     ) ENGINE=MyISAM;");
+   }
+   
+   public function uninstall(){
+   
+   }
+}
