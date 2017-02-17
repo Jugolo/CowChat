9f7201d53df1c1718c754d5e5f7ba35f44f27c55
Ronnie <rix17172@gmail.com>
2017-02-17 20:36:11 +0100

    Update plugin.php

--- lib/plugin/Friends/plugin.php
+++ lib/plugin/Friends/plugin.php
@@ -7,6 +7,19 @@ class Friends_Plugin{
       $this->db = $db;
    }
    
+   public function events(){
+     return [
+       new PluginEventList("trigger", "system.user.login", "userlogin")
+     ];
+   }
+   
+   public function userlogin(User $user){
+     $query = $this->db->query("SELECT * FROM `".DB_PREFIX."chat_friends` WHERE `one`='".$user->id()."' OR `two`='".$user->id()."'");
+     while($row = $query->get()){
+       bot_self_other($row['one'] == $user->id() ? $row['two'] : $row['one'], 1, "/friend login ".$user->nick());
+     }
+   }
+   
    public function doInstall(){//trigged when the install is installed
      $this->db->query("CREATE TABLE IF NOT EXISTS `".DB_PREFIX."chat_friends` (
        `one` int(11) NOT NULL,
