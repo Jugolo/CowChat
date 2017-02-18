ae2a282c0de840d288eba43d2c4b0a7a9362f80b
Ronnie <rix17172@gmail.com>
2017-02-18 22:28:28 +0100

    Update plugin.php

--- lib/plugin/Friends/plugin.php
+++ lib/plugin/Friends/plugin.php
@@ -10,8 +10,32 @@ class Friends_Plugin{
    public function events(){
      return [
        new PluginEventList("trigger", "system.user.login",     "userlogin"),
-       new PluginEventList("trigger", "client.javascript.end", "javascriptend")
+       new PluginEventList("trigger", "client.javascript.end", "javascriptend"),
+       new PluginEventList("command", "addfriend",             "addfriend")
      ];
+   }
+   
+   public function addfriend(User $user, PostData $post){
+      $command = $post->getMessage();
+      if(trim($command) == "/addfriend"){
+         bot_self($post->id(), "[color=red]Missing username to add to frind list[/color]");
+         return;
+      }
+      
+      $command = substr($command, 11);
+      $id = $this->db->query("SELECT `id` FROM `".DB_PREFIX."chat_user` WHERE `nick`='".$this->db->clean($command)."'")->get();
+      if(!$id){
+         bot_self($post->id(), "[color=red]Unknown user[/color]");
+         return;
+      }
+      $this->db->query("INSERT INTO `".DB_PREFIX."chat_friends` (
+        `one`,
+        `two`
+      ) VALUES (
+        '".$user->id()."',
+        '".$id."'
+      );");
+      bot_self($post->id(), "[color=green]The user is now you friend[/color]");
    }
    
    public function javascriptend(){
@@ -49,6 +73,7 @@ class Friends_Plugin{
        `one` int(11) NOT NULL,
        `two` int(11) NOT NULL
      ) ENGINE=MyISAM;");
+     bot_self(1, "Thanks to install friends plugin. This plugin is not done yet but please go to github to help to get it done");
    }
    
    public function uninstall(){
