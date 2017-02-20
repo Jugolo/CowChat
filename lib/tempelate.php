633e109678364912b5b6f4fd3fb0430f45983918
Ronnie <rix17172@gmail.com>
2017-02-20 14:42:27 +0100

    Update tempelate.php

--- lib/tempelate.php
+++ lib/tempelate.php
@@ -1,1 +1,16 @@
-
+<?php
+class Tempelate{
+  public function parse($file) : bool{
+    if(!file_exists($file)){
+      return true;
+    }
+    $source = file_get_contents($file);
+    $preg = preg_match_all("/@-([a-z]*) (.*?[^-@])-@/", $source, $reg);
+    for($i=0;$i<$preg;$i++){
+      switch($reg[1][$i]){
+          
+      }
+    }
+    echo $source;
+  }
+}
