b12b58390faac43b3d23ee373e0569fc484881cf
Ronnie <rix17172@gmail.com>
2017-02-20 20:07:30 +0100

    Update tempelate.php

--- lib/tempelate.php
+++ lib/tempelate.php
@@ -3,14 +3,17 @@ class Tempelate{
   private $path;
   
   public function path(string $dir){
+    if(!preg_match("/\/$/", $dir)){
+      $dir .= "/";
+    }
     $this->path = $dir;
   }
   
   public function parse($file) : bool{
-    if(!file_exists($file)){
+    if(!file_exists($this->dir.$file)){
       return false;
     }
-    $source = file_get_contents($file);
+    $source = file_get_contents($this->dir.$file);
     $preg = preg_match_all("/@-([a-z]*) (.*?[^-@])-@/", $source, $reg);
     for($i=0;$i<$preg;$i++){
       switch($reg[1][$i]){
