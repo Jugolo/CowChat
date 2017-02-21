9377bd3a4ec3aac6221f2766bb4963d2b23c6b0d
Ronnie <rix17172@gmail.com>
2017-02-21 09:46:31 +0100

    Update tempelate.php

--- lib/tempelate.php
+++ lib/tempelate.php
@@ -22,10 +22,10 @@ class Tempelate{
     $source = file_get_contents($this->part.$file);
     $arg = $this->render($source, $i);
     //controle if got to the end and the render not return true. true is return when @-end-@
-    if(strlen($source)-1 > $i || $arg === true){
+    if(strlen($source)-1 > $i || $arg["type"] === "block"){
       return false;
     }
-    echo $arg;
+    echo $arg["source"];
     return true;
   }
     
@@ -40,7 +40,7 @@ class Tempelate{
           return false;
         }
         if($block == "end"){
-          return true;
+          return ["type" => "block", "source" => $buffer];
         }
         $pos = strpos($block, " ");
         switch($pos !== false ? substr($block, 0, $pos) : ""){
@@ -51,12 +51,33 @@ class Tempelate{
               $lang = $this->lang[$code];
             }
             $buffer .= $this->lang[$code];
+          break;
+          case "include":
+          $item = explode(".", substr($block, $pos+1));
+          $f = array_pop($item);
+          $dir = $this->path;
+          for($d=0;$d<count($item);$d++){
+            if(trim($item[$d]) == "" || !file_exists($dir.$item[$d])){
+              return false;
+            }
+            $dir .= $item[$d]."/";
+          }
+          
+          if(!file_exists($dir.$f.".style")){
+            return false;
+          }
+          $s = $this->render(file_get_contents($dir.$f.".style"));
+          if(!$s || $s["type"] != "code"){
+            return false;
+          }
+          $buffer .= $s["source"]
+          break;
         }
       }else{
         $buffer .= $source[$i];
       }
     }
-    return $buffer;
+    return ["type" => "code", "source" => $buffer];
     $preg = preg_match_all("/@-(@?[a-z]*) (.*?[^-@])-@/", $source, $reg);
     for($i=0;$i<$preg;$i++){
       switch($reg[1][$i]){
