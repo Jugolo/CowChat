88de99629354941a356681bcbe2c12e7382751d7
Ronnie <rix17172@gmail.com>
2017-03-13 00:42:14 +0100

    Update language.php

--- lib/language.php
+++ lib/language.php
@@ -1,1 +1,46 @@
 <?php
+class Language{
+  private static $langCode;
+  
+  private static function detectLanguage(){
+    $available_languages = array_flip($this->getLanguageList());
+
+    $langs;
+    preg_match_all('~([\w-]+)(?:[^,\d]+([\d.]+))?~', strtolower($_SERVER["HTTP_ACCEPT_LANGUAGE"]), $matches, PREG_SET_ORDER);
+    foreach($matches as $match) {
+
+        list($a, $b) = explode('-', $match[1]) + array('', '');
+        $value = isset($match[2]) ? (float) $match[2] : 1.0;
+
+        if(isset($available_languages[$match[1]])) {
+            $langs[$match[1]] = $value;
+            continue;
+        }
+
+        if(isset($available_languages[$a])) {
+            $langs[$a] = $value - 0.1;
+        }
+
+    }
+    arsort($langs);
+
+    $langs = array_keys($langs);
+    if(count($langs) == 0){
+      return null;
+    }
+    
+    return $langs[0];
+  }
+  
+  private function getLanguageList() : array{
+    $ress = opendir("./locale");
+    $buffer = [];
+    while($name = readdir($ress)){
+      if($name == "." && $name == ".." && is_dir("./locale/".$name)){
+        $buffer[] = $name;
+      }
+    }
+    
+    return $buffer;
+  }
+}
