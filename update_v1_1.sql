787f503ab6ea7898b005bc91bc3a9bc199eb9b7f
Ronnie <rix17172@gmail.com>
2017-03-13 11:59:06 +0100

    Update update_v1_1.sql

--- update_v1_1.sql
+++ update_v1_1.sql
@@ -1,1 +1,8 @@
 ALTER TABLE `%prefix%chat_name` ADD `members` int(11) NOT NULL AFTER `title`;
+
+CREATE TABLE IF NOT EXISTS `%prefix%chat_updater` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `dir` varchar(255) NOT NULL,
+  `last_check` int(11) NOT NULL,
+  PRIMARY KEY(`id`)
+  ) ENGINE=MyISAM;
