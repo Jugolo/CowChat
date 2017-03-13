b715d3458f60310c51884166c8c67b68ac2c942b
Ronnie <rix17172@gmail.com>
2017-03-13 12:22:10 +0100

    Update update_v1_1.sql

--- update_v1_1.sql
+++ update_v1_1.sql
@@ -2,6 +2,7 @@ ALTER TABLE `%prefix%chat_name` ADD `members` int(11) NOT NULL AFTER `title`;
 
 CREATE TABLE IF NOT EXISTS `%prefix%chat_updater` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
+  `version` varchar(255) NOT NULL,
   `dir` varchar(255) NOT NULL,
   `last_check` int(11) NOT NULL,
   PRIMARY KEY(`id`)
