52c89f3007ce2e427f35ebdde28cfcc9559c579c
Ronnie <rix17172@gmail.com>
2017-03-13 14:50:11 +0100

    Update updater.php

--- lib/updater.php
+++ lib/updater.php
@@ -58,6 +58,7 @@ class Updater{
     curl_setopt($curl, CURLOPT_URL, $url);
     curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
     curl_setopt($curl, CURLOPT_USERAGENT, "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1)");
+    curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
     $source = curl_exec($curl);
     if(!$source){
       return null;
