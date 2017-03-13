5ce6f18bca699d109efd8cc7ee0b5fc910f8b190
Ronnie <rix17172@gmail.com>
2017-03-13 11:32:00 +0100

    Update log.php

--- lib/log.php
+++ lib/log.php
@@ -3,13 +3,20 @@
 //it append a system log and a channel log.
 
 function system_log($message){
+  if(!Config::get("enable_systemlog")){
+    return;
+  }
   $fopen = fopen("./lib/log/system.log", "a+");
   $size = fstat($fopen)["size"];
-  $dateString = "-----[".date("d/m-Y")."]-----";
+  $dateString = "-----[".date("d/m-Y")."]-----\r\n";
   if($size != 0){
     if(strpos(fread($fopen, $size), $dateString) === false){
       fwrite($fopen, "\r\n".$dateString);
+    }else{
+      fwrite($fopen, "\r\n");
     }
+  }else{
+    fwrite($fopen, $dateString);
   }
   fwrite($fopen, ($size == 0 ? "" : "\r\n")."[".date("s:i:H")."]".$message);
   fclose($fopen);
