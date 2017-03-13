b60e9e161386f445317b5c85277fd2d244cd6530
Ronnie <rix17172@gmail.com>
2017-03-13 11:58:49 +0100

    Update update_v1_0.sql

--- update_v1_0.sql
+++ update_v1_0.sql
@@ -9,3 +9,10 @@ CREATE TABLE IF NOT EXISTS `%prefix%chat_plugin` (
   `method` varchar(255) NOT NULL,
   PRIMARY KEY(`id`)
 ) ENGINE=MyISAM;
+
+CREATE TABLE IF NOT EXISTS `%prefix%chat_updater` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `dir` varchar(255) NOT NULL,
+  `last_check` int(11) NOT NULL,
+  PRIMARY KEY(`id`)
+  ) ENGINE=MyISAM;
