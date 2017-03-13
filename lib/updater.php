635177b79cf28924e4ed9d7900dd8484b4f28d85
Ronnie <rix17172@gmail.com>
2017-03-13 13:53:10 +0100

    Update updater.php

--- lib/updater.php
+++ lib/updater.php
@@ -18,11 +18,28 @@ class Updater{
   }
   
   private static function getCurrentVersion($data){
-    $data = self::request("https://api.github.com/repos/Jugolo/CowChat/tags");
+    $data = self::request("https://api.github.com/repos/".$data["owner"]."/".$data["repo"]."/tags");
+    if(!$data){
+      return $data["version"];
+    }
+    $n = "V0.0";
+    foreach($data as $item){
+      if(version_compare($n, $item["name"], '<')){
+        $n = $item["name"];
+      }
+    }
+    
+    return $n;
   }
   
   private static request($url){
     $curl = curl_init();
-    curl_setopt($ch, CURLOPT_URL, "https://api.github.com/re);
+    curl_setopt($curl, CURLOPT_URL, $url);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+    $source = curl_exec($curl);
+    if(!$source){
+      return null;
+    }
+    return json_decode($source, true);
   }
 }
