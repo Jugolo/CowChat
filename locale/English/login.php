8e67775c10e7fb8517ff07d76e7803bf38d9b897
Ronnie <rix17172@gmail.com>
2017-03-13 10:12:13 +0100

    Update login.php

--- locale/English/login.php
+++ locale/English/login.php
@@ -1,1 +1,17 @@
 <?php
+$locale = array();
+$locale["title"]                = "Welcommen to out chat - Login";
+$locale["login_title"]          = "Login";
+$locale["online_user"]          = "Online user";
+$locale["login"]                = "Login";
+$locale["username"]             = "Username";
+$locale["password"]             = "Password";
+$locale["re_password"]          = "Password again";
+$locale["login_now"]            = "Login now";
+$locale["create_account"]       = "Create account";
+$locale["missing_username"]     = "Missing username";
+$locale["missing_password"]     = "Missing password";
+$locale["wrong_login"]          = "Wrong username and/or password";
+$locale["missing_re_password"]  = "Missing repeat of password";
+$locale["password_mismatch"]    = "The two password are not equel";
+$locale["username_taken"]       = "Username is taken";
