a21b7ce350e751366ec4e882da93e0cc6505c5a1
Ronnie <rix17172@gmail.com>
2017-03-13 13:27:08 +0100

    Update update_v1_1.sql

--- update_v1_1.sql
+++ update_v1_1.sql
@@ -5,5 +5,7 @@ CREATE TABLE IF NOT EXISTS `%prefix%chat_updater` (
   `version` varchar(255) NOT NULL,
   `dir` varchar(255) NOT NULL,
   `last_check` int(11) NOT NULL,
+  `owner` varchar(255) NOT NULL,
+  `repo` varchar(255) NOT NULL,
   PRIMARY KEY(`id`)
   ) ENGINE=MyISAM;
