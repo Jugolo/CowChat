60da6b3bac871666055f1fa3052b75247416caa5
Ronnie <rix17172@gmail.com>
2017-03-13 13:41:15 +0100

    Update updater.php

--- lib/updater.php
+++ lib/updater.php
@@ -1,6 +1,28 @@
 <?php
 class Updater{
   public static function controle(DatabaseHandler $db){
-    
+    $query = $database->query("SELECT * FROM `".DB_PREFIX."chat_updater` WHERE `last_check`<'".time()."'");
+    while($row = $query->get()){
+      if(self::needUpdate($row)){
+        
+      }
+    }
+  }
+  
+  private static function needUpdate(array $data) : bool{
+    $current = self::getCurrentVersion($data);
+    if(!$current){
+      return false;
+    }
+    return version_compare($data["version"], $current, '<');
+  }
+  
+  private static function getCurrentVersion($data){
+    $data = self::request("https://api.github.com/repos/Jugolo/CowChat/tags");
+  }
+  
+  private static request($url){
+    $curl = curl_init();
+    curl_setopt($ch, CURLOPT_URL, "https://api.github.com/re);
   }
 }
