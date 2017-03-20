42a9acdf8b9f2d06a48ea08d731e3658c8ae58e7
Ronnie <rix17172@gmail.com>
2017-03-20 14:53:49 +0100

    Update PasswordRecovery.php

--- Inc/User/PasswordRecovery.php
+++ Inc/User/PasswordRecovery.php
@@ -2,6 +2,8 @@
 namespace Inc\User;
 
 use Request;
+use Inc\Random;
+use Inc\Mailer;
 
 class PasswordRecovery{
   const RECOVERY_GET = "npRequest";
@@ -11,5 +13,25 @@ class PasswordRecovery{
       return self::handleRecovery();
     }
     return false;
+  }
+  
+  public static function handleRecovery(){
+    global $server;
+    //let us try to get the user!
+    $db = $server->getDatabase();
+    
+    $data = $db->query("SELECT `id`, `username`, `email` FROM `".DB_PREFIX."chat_user` WHERE `email`='".$db->query(Request::get("email"))."' AND `status`='N'")->get();
+    if(!$data){
+      return false;
+    }
+    $password = Random::string(12);
+    $mail = new Mailer();
+    $mail->selectTempelate("newpassword");
+    $mail->setArg("username", $data["username"]);
+    $mail->setArg("password", $data["password"]);
+    $mail->send($data["email"]);
+    
+    $db->query("UPDATE `".DB_PREFIX."chat_user` SET `password`='".$db->clean(sha1($password))."' WHERE `id`='".$data["id"]."'");
+    return true;
   }
 }
