a6e96b1273cee1831c839299402c673e505582ae
Ronnie <rix17172@gmail.com>
2017-03-20 14:28:41 +0100

    Update PasswordRecovery.php

--- Inc/User/PasswordRecovery.php
+++ Inc/User/PasswordRecovery.php
@@ -1,6 +1,15 @@
 <?php
 namespace Inc\User;
 
-class PasswordRecovery{
+use Request;
 
+class PasswordRecovery{
+  const RECOVERY_GET = "npRequest";
+  
+  public static isRecoveryRequest() : bool{
+    if($token = Request::get(self::RECOVERY_GET)){
+      return self::handleRecovery($token);
+    }
+    return false;
+  }
 }
