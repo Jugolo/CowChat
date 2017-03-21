ea528a39cc3c90f1a86c61c01aad03fe53f6f056
Ronnie <rix17172@gmail.com>
2017-03-21 09:01:14 +0100

    Update Random.php

--- Inc/Random.php
+++ Inc/Random.php
@@ -1,5 +1,12 @@
 <?php
+namespace Lib;
 
 class Random{
-
+  public static function string(int $length) : string{
+    $buffer = "";
+    for($i=0;$i<$length;$i++){
+      $buffer .= chr(mt_rand(33, 127));
+    }
+    return $buffer;
+  }
 }
