ace228c7cca7b82d0dfa119e0a9d581a60cf8144
Ronnie <rix17172@gmail.com>
2017-03-21 09:21:34 +0100

    Create MailTempelate.php

---
+++ Inc/Mail/MailTempelate.php
@@ -0,0 +1,12 @@
+<?php
+namespace Inc\Mail;
+
+class MailTempelate{
+  private $header = [];
+  private $subject = "Please do not replay to this mail";
+  private $message = "";
+  
+  public function __construct(string $file){
+  
+  }
+}
