6cda36a5aa7ee43e7fa8603f2d733ae43c35967f
Ronnie <rix17172@gmail.com>
2017-03-21 09:28:10 +0100

    Update MailTempelate.php

--- Inc/Mail/MailTempelate.php
+++ Inc/Mail/MailTempelate.php
@@ -7,6 +7,24 @@ class MailTempelate{
   private $message = "";
   
   public function __construct(string $file){
-  
+     $fopen = fopen($file, "r");
+    while($line = fgets($fopen)){
+      if(trim($line) == ""){
+        break;
+      }
+      
+      $pos = strpos($line, ": ");
+      if($pos == -1){
+        continue;
+      }
+      $key = substr($line, 0, $pos);
+      if($key == "Subject"){
+        $this->subject = substr($line, $pos+2);
+      }else{
+        $this->header[$key] = substr($line, $pos+2);
+      }
+    }
+    while($char = fread($fopen, 1))
+      $this->message .= $char;
   }
 }
