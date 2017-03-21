168ffb38563706693c13e8040c3bae21af8ef4fc
Ronnie <rix17172@gmail.com>
2017-03-21 09:09:22 +0100

    Update Mailer.php

--- Inc/Mailer.php
+++ Inc/Mailer.php
@@ -4,4 +4,12 @@ namespace Lib;
 class Mailer{
   private $args = [];
   private $tempelate;
+  
+  public function selectTempelate(string $tempelate) : bool{
+    if(!file_exists($tempelate)){
+      return false;
+    }
+    $this->tempelate = $tempelate;
+    return true;
+  }
 }
